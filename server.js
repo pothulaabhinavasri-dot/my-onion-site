bbaa56ef149be053188375dc6bcdbcd388ec5684
pothulaabhinavasri-dot <pothulaabhinavasri@gmail.com>
2026-01-07 22:16:07 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-app.use(express.static(__dirname));
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("frontend"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log("Server running on port", $PORT);
 });
